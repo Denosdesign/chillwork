44f70c4bcf3ca3e6b777398d6dd71259a6f60494
Deno <46556615+Denosdesign@users.noreply.github.com>
2026-08-21 17:25:33 +0800

    Add Still icon scripts

---
+++ still/assets/useNavigate-DfVbYbWF.js
@@ -0,0 +1,1 @@
+import{L as e,bt as t,vt as n}from"./store-Bqb__SR8.js";var r=t(n(),1);function i(t){let n=e();return r.useCallback(e=>n.navigate({...e,from:e.from??t?.from}),[t?.from,n])}export{i as t};
